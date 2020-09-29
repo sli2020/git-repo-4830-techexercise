0fa3a81f603b95ff92ea80ca06e8670a30dd9012
li_shihang <li_shihang@DESKTOP-1ERDMVI>
2020-09-28 19:07:40 -0500

    Changed Hello World to a header.

--- webproject001/src/MyServlet.java
+++ webproject001/src/MyServlet.java
@@ -27,7 +27,7 @@ public class MyServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Hello World!");
+		response.getWriter().append("<h1>Hello World!</h1>");
 	}
 
 	/**
